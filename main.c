17a7f23f161b99d588cd5ca949522b7427f61878
Jack <tayloj22@tcnj.edu>
2020-03-05 17:36:19 -0500

    added threading implementation

--- main.c
+++ main.c
@@ -5,37 +5,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int **get_input() {
-	FILE *f = fopen("input.txt", "r");
-	if (f==NULL) 
-		printf("Error Opening input.txt\n");
-	
-	int nums[9][9];
-	int d; 
-	fscanf(f, "%d", d);
-	print("%d", d);
-	/*
-	for (int i=0; i<9; i++) {
-		for (int j=0; j<9; j++) {
-			fscanf(f, "%d", &nums[i][j]);
-		}
-	}
-	*/
-	fclose(f);
-	return nums;
-}
+
 
 int main (int argc, char** argv) {
-	int** arr = getInput(); //initialize array with getInput() method
-	int N = 11; //integer specifying # of threads
+	int** arr = getInput(); //initialize array using getInput method
 
-	pthread_t tid[N];
+	pthread_t tid[11]; //declare N threads
 
-	for (int i = 0;
-}
+	for (int i = 0; i < 11; i++) {
+		if (i == 0) {
+			pthread_create(&tid[i], 0, getCol, NULL); //create threads for checking columns
+		}
+		else if (i == 1) {
+			pthread_create(&tid[i], 0, getRow, NULL); //create thread for checking rows
+		}
+		else {
+			pthread_create(&tid[i], 0, getBox, NULL); //create threads for checking boxes
+		}
+	}
 
-void luke() {
-	int n = get_input();
+	for (int i = 0; i < 11; i++) {
+		pthread_join(tid[i], NULL); //join all 11 threads
+	}
+
+	return 0;
 }
 
 int method() {
