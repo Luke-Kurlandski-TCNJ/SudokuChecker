5f116a001e6d8d35dd24d9dd1439f1114bf2f1a3
Jack <tayloj22@tcnj.edu>
2020-03-05 17:31:53 -0500

    implemented threading

--- main.c
+++ main.c
@@ -6,7 +6,12 @@
 #include <unistd.h>
 
 int main (int argc, char** argv) {
-	
+	int** arr = getInput(); //initialize array with getInput() method
+	int N = 11; //integer specifying # of threads
+
+	pthread_t tid[N];
+
+	for (int i = 0;
 }
 
 int method() {
